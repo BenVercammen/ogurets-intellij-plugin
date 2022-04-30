42e05bc2565475d047bdef4c1522ee7fd9258caf
GitHub Action <action@github.com>
2022-04-30 09:01:44 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/benvercammen/oguretsintellijplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.benvercammen.oguretsintellijplugin.services
+
+import com.github.benvercammen.oguretsintellijplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
